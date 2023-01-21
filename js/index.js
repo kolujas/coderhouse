ae2f9954782b187a51ac676aa9bc6f9c82cb0c05
kolujAs <fernandodeibe@gmail.com>
2023-01-21 08:57:03 -0300

    implentacion de inputs en el html

--- js/index.js
+++ js/index.js
@@ -1,153 +1,14 @@
-// Primera version
-
-// let cont = 0;
-// let res = 0;
-
-
-// calcularPromedio = (nota, nota2, nota3) => {
-//   nota = parseFloat(prompt("Ingrese su nota 1:"))
-//   nota2 = parseFloat(prompt("Ingrese su nota 2:"))
-//   nota3 = parseFloat(prompt("Ingrese su nota 3:"))
-//   while (cont === 0) {
-//     if (nota > 10) {
-//       console.log("Los campos deben ser completados por un número válido entre 1 al 10");
-//       return false;
-//     }
-//     if (nota2 > 10) {
-//       console.log("Los campos deben ser completados por un número válido entre 1 al 10");
-//       return false;
-//     }
-//     if (nota3 > 10) {
-//       console.log("Los campos deben ser completados por un número válido entre 1 al 10");
-//       return false;
-//     }
-
-//     res = (nota + nota2 + nota3) / 3;
-//     cont++;
-//   }
-
-//   validacion()
-// }
-
-// validacion = () => {
-//   if (isNaN(res)) {
-//     console.log("Los campos deben ser de formato numérico");
-//   } else if (res >= 7) {
-//     console.log("Tu promedio es: " + res + " felicidades sos un héroe!");
-//   } else {
-//     console.log("Tu promedio es: " + res + " sos un asistente!");
-//   }
-
-// }
-
-// calcularPromedio();
-
-
-// version 1.1
-
-// let cont = 1;
-// let res = 0;
-
-
-// calcularPromedio = (nota) => {
-
-//   nota = parseFloat(prompt("Ingrese su nota"))
-
-//   while (cont < 3) {
-
-//     nota += parseFloat(prompt("Ingrese su nota:"))
-
-//     res = nota / 3;
-
-//     if (res > 10) {
-//       console.log("Los campos deben ser completados por un número válido entre 1 al 10");
-//       return false;
-//     }
-
-//     cont++;
-//   }
-
-//   validacion()
-// }
-
-// validacion = () => {
-//   if (isNaN(res)) {
-//     console.log("Los campos deben ser de formato numérico");
-//   } else if (res >= 7) {
-//     console.log("Tu promedio es: " + res + " felicidades sos un héroe!");
-//   } else {
-//     console.log("Tu promedio es: " + res + " sos un asistente!");
-//   }
-
-// }
-
-// calcularPromedio();
-
-
-
-// console.log(pruebini);
-
-
-// let cont = 1;
-// let resultado = 0;
-// let ListadoDeAlumnos = [];
-
-// function Alumnos(nombre, resultado) {
-
-//   nombre = prompt("Ingresa tu nombre");
-//   nota = parseFloat(prompt("Ingrese su nota:"))
-
-//   while (cont <= 3) {
-
-//     this.nombre = nombre;
-//     this.resultado = resultado;
-
-//     nota += parseFloat(prompt("Ingrese su nota:"))
-
-
-//     resultado = nota / 3;
-
-//     console.log(nota);
-//     if (resultado > 10) {
-//       console.log("Los campos deben ser completados por un número válido entre 1 al 10");
-//       return false;
-//     }
-
-//     cont++;
-//   }  
-
-// }
-
-// let Alumno = new Alumnos();
-
-// ListadoDeAlumnos.push(Alumno.nombre, Alumno.resultado);
-
-// console.log(ListadoDeAlumnos);
-
-
-
-
-// version 1.2
-
-// SEGUNDA ENTREGA
-
 let arr = [];
 let nota = 0;
 let cont = 1;
 let res = 0;
 
 
-const datos = document.querySelector('.datos');
-const nombre = document.querySelector('.nombre');
-const promedio = document.querySelector('.promedio');
-
-
+let nombreAlumno = document.querySelector('#nombreAlumno');
+let notaAlumno = document.querySelector('#notaAlumno');
+let calcularBtn = document.querySelector('#calcularBtn');
 
 function Alumnos(nombre, nota, res) {
-
-
-  nombre = prompt("Ingrese su nombre");
-  nota = parseFloat(prompt("Ingrese su nota:"))
 
   validarMayorA10();
 
@@ -155,25 +16,48 @@ function Alumnos(nombre, nota, res) {
   this.nota = nota;
   this.res = res;
 
-  while (cont < 3) {
 
-    nota += parseFloat(prompt("Ingrese su nota:"))
+  notaAlumno.addEventListener("change", () => {
     
-    validarMayorA10();
+    console.log("nota cambiada");
+  });
 
-    this.res = nota / 3; 
-    
-    cont++;
-  }
 
-  if (isNaN(this.res)) {
-    console.log("Los campos deben ser de formato numérico");
-    return false;
-  } else if (this.res >= 7) {
-    console.log(`Tu promedio es: ${this.res} felicidades sos un héroe!`);
-  } else {
-    console.log("Tu promedio es: ${this.res} sos un asistente!");
-  }
+  calcularBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    nombre = nombreAlumno.value;
+    nota = notaAlumno.value   
+
+    // const input = document.createElement('input')
+    // padre.append(input)
+    while (cont < 3) {
+      nombreAlumno.disabled = true;
+      notaAlumno.value = ""
+      
+      notaAlumno =+ nota.value;
+      validarMayorA10();
+  
+      this.res = nota / 3;       
+      cont++;
+      
+    }
+
+    if (isNaN(this.res)) {
+      console.log("Los campos deben ser de formato numérico");
+      return false;
+    } else if (this.res >= 7) {
+      console.log(`Tu promedio es: ${this.res} felicidades sos un héroe!`);
+    } else {
+      console.log(`Tu promedio es: ${this.res} sos un asistente!`);
+    }
+
+    console.log(nombre)
+    console.log(this.res)
+  })
+
+  
+
+ 
 
 
   // datos guardados en LS
@@ -196,7 +80,6 @@ const validarMayorA10 = () =>{
 
 
   let alumno = new Alumnos(Alumnos.nombre, Alumnos.res);
-  // console.log(`Nombre: ${alumno.nombre} - Promedio del alumno: ${alumno.res}`);
   nombre.innerHTML = `Alumno: ${alumno.nombre}`;
   promedio.innerHTML = `Tu promedio es: ${alumno.res}`;
 
@@ -250,23 +133,4 @@ usuariosBtn.addEventListener("click", () => {
   document.getElementById('users').innerHTML = listadoUsuarios.join('');
 })
 
-
-
-// const Listado = () => {
-
-//   // const nombresBD = bd.map((el) => el.nombre);
-//   // // console.log(`Listado de alumnos: ${nombresBD}`);
-//   // let array = nombresBD;
-//   // array = {...array}  
-
- 
-//   let listadoUsuarios = db.map(function(user){
-//     return '<li>' + user.nombre + '</li>';
-//   })
-
-//   document.getElementById('users').innerHTML = listadoUsuarios.join('');
-// }
-
-
-// Listado();
 
